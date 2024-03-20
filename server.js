97fe5b94a9902a2cedac069a205c8aeb41497870
grantdrink <grantjdrinkwater@gmail.com>
2024-03-20 13:01:05 -0400

    bal

--- server.js
+++ server.js
@@ -176,8 +176,8 @@ app.post("/exchangePublicToken", async function (request, response, next) {
 // Fetches balance data using the Node client library for Plaid
 app.post("/Balance", async (req, res, next) => {
   console.log("Start of bal req")
-  console.log(req.body)
-  const { access_token } = req.body;
+  console.log(req)
+  const access_token = req.body;
   console.log("Acces_token " + access_token)
   const balanceResponse = await client.accountsBalanceGet(access_token);
   const formattedResponse = balanceResponse.data; // Assuming balanceResponse is an Axios response object
