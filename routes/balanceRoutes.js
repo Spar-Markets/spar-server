073ca377e78c2390fba52d02040b6a590201d1bd
grantdrink <grantjdrinkwater@gmail.com>
2024-07-03 21:46:45 -0400

    Add Procfile for Heroku

--- routes/balanceRoutes.js
+++ routes/balanceRoutes.js
@@ -29,6 +29,7 @@ router.post("/updateUserBalanceWithdraw", async (req, res) => {
       );
     } catch (error) {
       console.log("user doesn't exist");
+      console.log("user doesn't existthis is logged twice as a test");
     }
   } catch (error) {
     console.error("Error in updating balance");
