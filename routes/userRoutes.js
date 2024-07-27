b88fc6138902561fd048f595c7fcab388edac6d8
grantdrink <grantjdrinkwater@gmail.com>
2024-07-27 12:33:51 -0400

    image

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose"); // Import mongoose
 
 const User = require("../models/User");
 const getLeftOfAtSymbol = require("../utility/getLeftOfAtSymbol.js");
