d70405fd6119165d2d9e670561ba4e45e8240d31
jjquaratiello <jjquaratiello@gmail.com>
2024-08-01 19:46:23 -0400

    image hasDefImage string -> boolean

--- models/User.js
+++ models/User.js
@@ -23,7 +23,7 @@ const userSchema = new mongoose.Schema({
     unique: false,
   },
   hasDefaultProfileImage: {
-    type: String,
+    type: Boolean,
     required: true,
     unique: false,
   },
