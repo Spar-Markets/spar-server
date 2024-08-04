2edde47a62c84e0ad343f3f22f5ff55fb663fafd
jjquaratiello <jjquaratiello@gmail.com>
2024-08-03 20:19:43 -0400

    milk

--- utility/getMostRecentMarketOpenDay.js
+++ utility/getMostRecentMarketOpenDay.js
@@ -5,7 +5,6 @@ function getMostRecentMarketOpenDay(now) {
   i = 0;
   while (!isMarketOpenToday(now)) {
     i += 1;
-    console.log("GetMostRecentMarketDay: Skip #" + 1, "this is not a market day:", now)
     now = getPreviousDay(now);
   }
   return now;
