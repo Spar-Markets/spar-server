833041cc11a2eb135fbe4d297b9ae5fef66ca446
jjquaratiello <jjquaratiello@gmail.com>
2024-08-15 16:01:03 -0400

    commit

--- routes/waitListRoutes.js
+++ routes/waitListRoutes.js
@@ -2,7 +2,10 @@ const express = require("express");
 const router = express.Router();
 const WaitListUser = require("../models/WaitListUser.js");
 const nodemailer = require("nodemailer");
-const { appPassword } = require("../config/constants.js");
+const { appPassword, mailKey } = require("../config/constants.js");
+const sgMail = require('@sendgrid/mail')
+sgMail.setApiKey(mailKey)
+
 
 
 router.post("/sendConfirmationEmail", async (req, res) => {
@@ -33,8 +36,35 @@ router.post("/sendConfirmationEmail", async (req, res) => {
     //TODO, point domain to spar server
     const confirmationUrl = `https://spar-server-heroku-0fbb9e6415f2.herokuapp.com/confirm-waitlist?email=${encodeURIComponent(email)}`;
 
+    const msg = {
+      to: email, // Change to your recipient
+      from: "jjquaratiello@gmail.com", // Change to your verified sender
+      subject: 'Spar Markets - Confirm Your Spot on the Waitlist',
+      text: 'text text',
+      html: `
+        <div style="font-family: Arial, sans-serif; padding: 20px; max-width: 600px; margin: auto;">
+          <div style="margin-top: 20px; text-align: left;">
+            <h2 style="color: black; margin: 0;">Confirm Your Spot on the Waitlist</h2>
+            <p>Thank you for signing up for our waitlist! Please confirm your spot by clicking the button below:</p>
+            <a href="${confirmationUrl}" style="display: block; width: 100%; background-color: white; color: black; padding: 15px 0; text-decoration: none; border: 2px solid black; border-radius: 5px; font-weight: bold; text-align: center;">Confirm Spot</a>
+            <p style="margin-top: 20px;">Note: do not join the waitlist with multiple emails. When you create your account, you'll have to verify your identity and we only allow one account per person.</p>
+            <p style="margin-top: 20px;">If you did not request to join the waitlist, please ignore this email.</p>
+          </div>
+        </div>
+      `,
+    }
+
+    sgMail
+      .send(msg)
+      .then(() => {
+        res.status(200).send({ message: "Confirmation email sent, please check your inbox." })
+      })
+      .catch((error) => {
+        res.status(500).send({ message: `Failed to send confirmation email, please try again later. ${error}` });
+      })
+
     // Email content
-    const mailOptions = {
+    /*const mailOptions = {
       to: email,
       subject: 'Spar Markets - Confirm Your Spot on the Waitlist',
       html: `
@@ -57,7 +87,7 @@ router.post("/sendConfirmationEmail", async (req, res) => {
         return res.status(500).send({ message: `Failed to send confirmation email, please try again later. ${error}` });
       }
       res.status(200).send({ message: "Confirmation email sent, please check your inbox." });
-    });
+    }); */
   } catch (error) {
     console.error("Error sending confirmation email:", error);
     res.status(500).send({ message: `Server error, please try again later. ${error}` });
