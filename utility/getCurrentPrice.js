59dffe552a128e74eedd75c1575e746a65bafbbf
jacksonbelanger <jacksonlbelanger@gmail.com>
2024-08-26 22:19:16 -0400

    milk

--- utility/getCurrentPrice.js
+++ utility/getCurrentPrice.js
@@ -3,8 +3,10 @@ const axios = require("axios"); // Corrected this line, do not destructure axios
 const { polygonKey } = require("../config/constants");
 
 async function getCurrentPrice(ticker) {
+  console.log("MILK GET CURRENT PRICE for ticker:", ticker);
   const url = `https://api.polygon.io/v2/snapshot/locale/us/markets/stocks/tickers/${ticker}?apiKey=${polygonKey}`;
   const response = await axios.get(url);
+  console.log("MILK RESPONSE:", response);
   const currPrice = response.data.ticker.day.c;
 
   return currPrice;
