fb980ca9d4b2ddd63486b38a62c875cda5c9cc00
Joseph Quaratiello <118370447+jjquaratiello@users.noreply.github.com>
2025-01-26 18:56:09 -0500

    Delete config directory

--- config/constants.js
+++
@@ -1,8 +0,0 @@
-const polygonKey = "Xr4ESsQJKShHeqROFIfTsVVvb9rppThs";
-
-serverUrl = "https://spar-server-heroku-0fbb9e6415f2.herokuapp.com";
-appPassword = "rnnq wuqj oolm hwyh"
-
-const mailKey = "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"
-
-module.exports = { polygonKey, serverUrl, appPassword, mailKey };
